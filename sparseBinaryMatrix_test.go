6f73e86c937368a4f955772efc29139751bd48d3
Zac Gross <zacgross@gmail.com>
2014-04-25 12:16:10 -0400

    added test for row replace on sparse binary matrix

--- sparseBinaryMatrix_test.go
+++ sparseBinaryMatrix_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 )
 
+//Tests getting/setting values
 func TestGetSet(t *testing.T) {
 
 	sm := NewSparseBinaryMatrix(10, 10)
@@ -21,6 +22,31 @@ func TestGetSet(t *testing.T) {
 
 	if sm.Get(7, 5) {
 		t.Errorf("Was true expected false @ [7,5]")
+	}
+
+}
+
+func TestRowReplace(t *testing.T) {
+	sm := NewSparseBinaryMatrix(10, 10)
+	sm.Set(2, 4, true)
+	sm.Set(6, 5, true)
+	sm.Set(7, 5, true)
+	sm.Set(8, 8, true)
+
+	if !sm.Get(8, 8) {
+		t.Errorf("Was false expected true @ [8,8]")
+	}
+
+	newRow := make([]bool, 10)
+	newRow[6] = true
+	sm.ReplaceRow(8, values)
+
+	if !sm.Get(8, 6) {
+		t.Errorf("Was false expected true @ [8,6]")
+	}
+
+	if sm.Get(8, 8) {
+		t.Errorf("Was true expected false @ [8,8]")
 	}
 
 }
