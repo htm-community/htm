3d5568dd0279499842f7e8a4a1ae57b820435fda
Zac Gross <zacgross@gmail.com>
2014-06-04 23:36:19 -0400

    implementing segment dutyCycle()

--- segment.go
+++ segment.go
@@ -27,6 +27,8 @@ type Segment struct {
 	lastPosDutyCycle          int
 	lastPosDutyCycleIteration int
 	syns                      []Synapse
+	dutyCycleTiers            []int
+	dutyCycleAlphas           []float64
 }
 
 //Creates a new segment
@@ -42,7 +44,101 @@ func NewSegment(tp *TemporalPooler, isSequenceSeg bool) *Segment {
 	seg.lastPosDutyCycle = 1.0 / tp.lrnIterationIdx
 	seg.lastPosDutyCycleIteration = tp.lrnIterationIdx
 
+	seg.dutyCycleTiers = []int{0, 100, 320, 1000,
+		3200, 10000, 32000, 100000,
+		320000}
+
+	seg.dutyCycleAlphas = []float64{0, 0.0032, 0.0010, 0.00032,
+		0.00010, 0.000032, 0.00001, 0.0000032,
+		0.0000010}
+
 	//TODO: initialize synapse collection
 
 	return &seg
+}
+
+// func (s *Segment) Equal(other *Segment) bool {
+// }
+
+/*
+Compute/update and return the positive activations duty cycle of
+this segment. This is a measure of how often this segment is
+providing good predictions.
+
+param active True if segment just provided a good prediction
+param readOnly If True, compute the updated duty cycle, but don't change
+the cached value. This is used by debugging print statements.
+
+returns The duty cycle, a measure of how often this segment is
+providing good predictions.
+
+**NOTE:** This method relies on different schemes to compute the duty cycle
+based on how much history we have. In order to support this tiered
+approach **IT MUST BE CALLED ON EVERY SEGMENT AT EACH DUTY CYCLE TIER**
+(ref dutyCycleTiers).
+
+When we don't have a lot of history yet (first tier), we simply return
+number of positive activations / total number of iterations
+
+After a certain number of iterations have accumulated, it converts into
+a moving average calculation, which is updated only when requested
+since it can be a bit expensive to compute on every iteration (it uses
+the pow() function).
+
+The duty cycle is computed as follows:
+
+dc[t] = (1-alpha) * dc[t-1] + alpha * value[t]
+
+If the value[t] has been 0 for a number of steps in a row, you can apply
+all of the updates at once using:
+
+dc[t] = (1-alpha)^(t-lastT) * dc[lastT]
+
+We use the alphas and tiers as defined in ref dutyCycleAlphas and
+ref dutyCycleTiers.
+*/
+func (s *Segment) dutyCycle(active, readOnly bool) float64 {
+
+	// For tier #0, compute it from total number of positive activations seen
+	if s.tp.lrnIterationIdx <= s.dutyCycleTiers[1] {
+		dutyCycle := s.positiveActivations / s.tp.lrnIterationIdx
+		if !readOnly {
+			s.lastPosDutyCycleIteration = s.tp.lrnIterationIdx
+			s.lastPosDutyCycle = dutyCycle
+		}
+		return dutyCycle
+	}
+
+	// How old is our update?
+	age := s.tp.lrnIterationIdx - s.lastPosDutyCycleIteration
+
+	//If it's already up to date, we can returned our cached value.
+	if age == 0 && !active {
+		return s.lastPosDutyCycle
+	}
+
+	alpha := 0.0
+	//Figure out which alpha we're using
+	for i := len(s.dutyCycleTiers); i > 0; i-- {
+		if s.tp.lrnIterationIdx > s.dutyCycleTiers[i] {
+			alpha = s.dutyCycleAlphas[i]
+			break
+		}
+	}
+
+	// Update duty cycle
+	dutyCycle := math.Pow(1.0-alpha, age) * s.lastPosDutyCycle
+
+	if active {
+		dutyCycle += alpha
+	}
+
+	// Update cached values if not read-only
+	if !readOnly {
+		s.lastPosDutyCycleIteration = s.tp.lrnIterationIdx
+		s.lastPosDutyCycle = dutyCycle
+	}
+
+	return dutyCycle
+
 }
