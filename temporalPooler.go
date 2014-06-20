d1e028e9616f08ed1ae1d286a226ddc3ab6fc4fb
Zac Gross <zacgross@gmail.com>
2014-06-20 00:16:44 -0400

    implemented inferBacktrack()

--- temporalPooler.go
+++ temporalPooler.go
@@ -531,3 +531,248 @@ func (tp *TemporalPooler) inferPhase1(activeColumns []int, useStartCells bool) b
 	return useStartCells || numPredictedColumns >= int(0.50*float64(len(activeColumns)))
 
 }
+
+/*
+ This "backtracks" our inference state, trying to see if we can lock onto
+the current set of inputs by assuming the sequence started up to N steps
+ago on start cells.
+
+@param activeColumns The list of active column indices
+
+This will adjust @ref infActiveState['t'] if it does manage to lock on to a
+sequence that started earlier. It will also compute infPredictedState['t']
+based on the possibly updated @ref infActiveState['t'], so there is no need to
+call inferPhase2() after calling inferBacktrack().
+
+This looks at:
+- @ref infActiveState['t']
+
+This updates/modifies:
+- @ref infActiveState['t']
+- @ref infPredictedState['t']
+- @ref colConfidence['t']
+- @ref cellConfidence['t']
+
+How it works:
+-------------------------------------------------------------------
+This method gets called from updateInferenceState when we detect either of
+the following two conditions:
+-# The current bottom-up input had too many un-expected columns
+-# We fail to generate a sufficient number of predicted columns for the
+next time step.
+
+Either of these two conditions indicate that we have fallen out of a
+learned sequence.
+
+Rather than simply "giving up" and bursting on the unexpected input
+columns, a better approach is to see if perhaps we are in a sequence that
+started a few steps ago. The real world analogy is that you are driving
+along and suddenly hit a dead-end, you will typically go back a few turns
+ago and pick up again from a familiar intersection.
+
+This back-tracking goes hand in hand with our learning methodology, which
+always tries to learn again from start cells after it loses context. This
+results in a network that has learned multiple, overlapping paths through
+the input data, each starting at different points. The lower the global
+decay and the more repeatability in the data, the longer each of these
+paths will end up being.
+
+The goal of this function is to find out which starting point in the past
+leads to the current input with the most context as possible. This gives us
+the best chance of predicting accurately going forward. Consider the
+following example, where you have learned the following sub-sequences which
+have the given frequencies:
+
+? - Q - C - D - E 10X seq 0
+? - B - C - D - F 1X seq 1
+? - B - C - H - I 2X seq 2
+? - B - C - D - F 3X seq 3
+? - Z - A - B - C - D - J 2X seq 4
+? - Z - A - B - C - H - I 1X seq 5
+? - Y - A - B - C - D - F 3X seq 6
+
+----------------------------------------
+W - X - Z - A - B - C - D <= input history
+^
+current time step
+
+Suppose, in the current time step, the input pattern is D and you have not
+predicted D, so you need to backtrack. Suppose we can backtrack up to 6
+steps in the past, which path should we choose? From the table above, we can
+see that the correct answer is to assume we are in seq 1. How do we
+implement the backtrack to give us this right answer? The current
+implementation takes the following approach:
+
+-# Start from the farthest point in the past.
+-# For each starting point S, calculate the confidence of the current
+input, conf(startingPoint=S), assuming we followed that sequence.
+Note that we must have learned at least one sequence that starts at
+point S.
+-# If conf(startingPoint=S) is significantly different from
+conf(startingPoint=S-1), then choose S-1 as the starting point.
+
+The assumption here is that starting point S-1 is the starting point of
+a learned sub-sequence that includes the current input in it's path and
+that started the longest ago. It thus has the most context and will be
+the best predictor going forward.
+
+From the statistics in the above table, we can compute what the confidences
+will be for each possible starting point:
+
+startingPoint confidence of D
+-----------------------------------------
+B (t-2) 4/6 = 0.667 (seq 1,3)/(seq 1,2,3)
+Z (t-4) 2/3 = 0.667 (seq 4)/(seq 4,5)
+
+First of all, we do not compute any confidences at starting points t-1, t-3,
+t-5, t-6 because there are no learned sequences that start at those points.
+
+Notice here that Z is the starting point of the longest sub-sequence leading
+up to the current input. Event though starting at t-2 and starting at t-4
+give the same confidence value, we choose the sequence starting at t-4
+because it gives the most context, and it mirrors the way that learning
+extends sequences.
+*/
+
+func (tp *TemporalPooler) inferBacktrack(activeColumns []int) {
+	// How much input history have we accumulated?
+	// The current input is always at the end of self._prevInfPatterns (at
+	// index -1), but it is also evaluated as a potential starting point by
+	// turning on it's start cells and seeing if it generates sufficient
+	// predictions going forward.
+	numPrevPatterns := len(tp.prevInfPatterns)
+	if numPrevPatterns <= 0 {
+		return
+	}
+
+	// This is an easy to use label for the current time step
+	currentTimeStepsOffset := numPrevPatterns - 1
+
+	// Save our current active state in case we fail to find a place to restart
+	// todo: save infActiveState['t-1'], infPredictedState['t-1']?
+	tp.DynamicState.infActiveStateBackup = tp.DynamicState.infActiveStateLast.Copy()
+
+	// Save our t-1 predicted state because we will write over it as as evaluate
+	// each potential starting point.
+	tp.DynamicState.infPredictedStateBackup = tp.DynamicState.infPredictedStateLast
+
+	// We will record which previous input patterns did not generate predictions
+	// up to the current time step and remove all the ones at the head of the
+	// input history queue so that we don't waste time evaluating them again at
+	// a later time step.
+	var badPatterns []int
+
+	// Let's go back in time and replay the recent inputs from start cells and
+	// see if we can lock onto this current set of inputs that way.
+
+	// Start the farthest back and work our way forward. For each starting point,
+	// See if firing on start cells at that point would predict the current
+	// input as well as generate sufficient predictions for the next time step.
+
+	// We want to pick the point closest to the current time step that gives us
+	// the relevant confidence. Think of this example, where we are at D and need
+	// to
+	// A - B - C - D
+	// decide if we should backtrack to C, B, or A. Suppose B-C-D is a high order
+	// sequence and A is unrelated to it. If we backtrock to B would we get a
+	// certain confidence of D, but if went went farther back, to A, the
+	// confidence wouldn't change, since A has no impact on the B-C-D series.
+
+	// So, our strategy will be to pick the "B" point, since choosing the A point
+	// does not impact our confidences going forward at all.
+	inSequence := false
+	candConfidence := -1.0
+	candStartOffset := 0
+
+	//for startOffset in range(0, numPrevPatterns):
+	for startOffset := 0; startOffset < numPrevPatterns; startOffset++ {
+		// If we have a candidate already in the past, don't bother falling back
+		// to start cells on the current input.
+		if startOffset == currentTimeStepsOffset && candConfidence != -1 {
+			break
+		}
+
+		// Play through starting from starting point 'startOffset'
+		inSequence = false
+		totalConfidence := 0.0
+		//for offset in range(startOffset, numPrevPatterns):
+		for offset := startOffset; offset < numPrevPatterns; offset++ {
+			// If we are about to set the active columns for the current time step
+			// based on what we predicted, capture and save the total confidence of
+			// predicting the current input
+
+			if offset == currentTimeStepsOffset {
+				for _, val := range activeColumns {
+					totalConfidence += tp.DynamicState.colConfidence[val]
+				}
+			}
+
+			// Compute activeState[t] given bottom-up and predictedState @ t-1
+			tp.DynamicState.infPredictedStateLast = tp.DynamicState.infPredictedState
+
+			inSequence = tp.inferPhase1(tp.prevInfPatterns[offset], (offset == startOffset))
+			if !inSequence {
+				break
+			}
+			// Compute predictedState at t given activeState at t
+			inSequence = tp.inferPhase2()
+			if !inSequence {
+				break
+			}
+
+		}
+
+		// If starting from startOffset got lost along the way, mark it as an
+		// invalid start point.
+		if !inSequence {
+			badPatterns = append(badPatterns, startOffset)
+			continue
+		}
+
+		// If we got to here, startOffset is a candidate starting point.
+		// Save this state as a candidate state. It will become the chosen state if
+		// we detect a change in confidences starting at a later startOffset
+		candConfidence = totalConfidence
+		candStartOffset = startOffset
+
+		if candStartOffset == currentTimeStepsOffset { // no more to try
+			break
+		}
+		tp.DynamicState.infActiveStateCandidate = tp.DynamicState.infActiveState.Copy()
+		tp.DynamicState.infPredictedStateCandidate = tp.DynamicState.infPredictedState.Copy()
+		tp.DynamicState.cellConfidenceCandidate = tp.DynamicState.cellConfidence.Copy()
+		copy(tp.DynamicState.colConfidenceCandidate, tp.DynamicState.colConfidence)
+		break
+
+	}
+
+	// If we failed to lock on at any starting point, fall back to the original
+	// active state that we had on entry
+	if candStartOffset == -1 {
+		tp.DynamicState.infActiveState = tp.DynamicState.infActiveStateBackup
+		tp.inferPhase2()
+	} else {
+		// Install the candidate state, if it wasn't the last one we evaluated.
+		if candStartOffset != currentTimeStepsOffset {
+			tp.DynamicState.infActiveState = tp.DynamicState.infActiveStateCandidate
+			tp.DynamicState.infPredictedState = tp.DynamicState.infPredictedStateCandidate
+			tp.DynamicState.cellConfidence = tp.DynamicState.cellConfidenceCandidate
+			tp.DynamicState.colConfidence = tp.DynamicState.colConfidenceCandidate
+		}
+
+	}
+
+	// Remove any useless patterns at the head of the previous input pattern
+	// queue.
+	for i := 0; i < numPrevPatterns; i++ {
+		if ContainsInt(i, badPatterns) || (candStartOffset != -1 && i <= candStartOffset) {
+			//pop prev pattern
+			tp.prevInfPatterns = tp.prevInfPatterns[:len(tp.prevInfPatterns)-1]
+		} else {
+			break
+		}
+	}
+
+	// Restore the original predicted state.
+	tp.DynamicState.infPredictedState = tp.DynamicState.infPredictedStateBackup
+}
