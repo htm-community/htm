19019a77507fdfe9ae719c65549ccee10da6b86e
Zac Gross <zacgross@gmail.com>
2014-06-23 23:22:30 -0400

    implemented learnPhase1()

--- temporalPooler.go
+++ temporalPooler.go
@@ -1231,3 +1231,90 @@ func (tp *TemporalPooler) getCellForNewSegment(colIdx int) int {
 
 	return candidateCellIdx
 }
+
+/*
+ Compute the learning active state given the predicted state and
+the bottom-up input.
+
+param activeColumns list of active bottom-ups
+param readOnly True if being called from backtracking logic.
+This tells us not to increment any segment
+duty cycles or queue up any updates.
+returns True if the current input was sufficiently predicted, OR
+if we started over on startCells. False indicates that the current
+input was NOT predicted, well enough to consider it as "inSequence"
+
+This looks at:
+- ref lrnActiveState['t-1']
+- ref lrnPredictedState['t-1']
+
+This modifies:
+- ref lrnActiveState['t']
+- ref lrnActiveState['t-1']
+
+*/
+
+func (tp *TemporalPooler) learnPhase1(activeColumns []int, readOnly bool) bool {
+
+	// Save previous active state and start out on a clean slate
+	tp.DynamicState.lrnActiveState.Clear()
+
+	// For each column, turn on the predicted cell. There will always be at most
+	// one predicted cell per column
+	numUnpredictedColumns := 0
+
+	for c := range activeColumns {
+		predictingCells := tp.DynamicState.lrnPredictedState.GetRowIndices(c)
+		numPredictedCells := len(predictingCells)
+		if numPredictedCells > 1 {
+			panic("number of predicted cells too high")
+		}
+
+		// If we have a predicted cell, turn it on. The segment's posActivation
+		// count will have already been incremented by processSegmentUpdates
+		if numPredictedCells == 1 {
+			i := predictingCells[0]
+			tp.DynamicState.lrnActiveState.Set(c, i, true)
+			continue
+		}
+
+		numUnpredictedColumns += 1
+		if readOnly {
+			continue
+		}
+
+		// If no predicted cell, pick the closest matching one to reinforce, or
+		// if none exists, create a new segment on a cell in that column
+		i, s, numActive := tp.getBestMatchingCell(c, tp.DynamicState.lrnActiveStateLast, tp.params.MinThreshold)
+
+		if s != nil && s.isSequenceSeg() {
+			tp.DynamicState.lrnActiveState.Set(c, i, true)
+			segUpdate := tp.getSegmentActiveSynapses(c, i, s, tp.DynamicState.lrnActiveStateLast, true)
+			s.totalActivations++
+			// This will update the permanences, posActivationsCount, and the
+			// lastActiveIteration (age).
+			trimSegment := segUpdate.adaptSegments(tp)
+
+			if trimSegment {
+				tp.trimSegmentsInCell(c, i, []Segment{s}, 0.00001, 0)
+			}
+
+		} else {
+			// If no close match exists, create a new one
+			// Choose a cell in this column to add a new segment to
+			i = tp.getCellForNewSegment(c)
+			tp.DynamicState.lrnActiveState.Set(c, i, true)
+			segUpdate := tp.getSegmentActiveSynapses(c, i, nil, tp.DynamicState.lrnActiveStateLast, true)
+			segUpdate.sequenceSegment = true
+			segUpdate.adaptSegments(tp) // No need to check whether perm reached 0
+		}
+
+		// Determine if we are out of sequence or not and reset our PAM counter
+		// if we are in sequence
+		numBottomUpColumns := len(activeColumns)
+
+		//true if in sequence, false if out of sequence
+		return numUnpredictedColumns < numBottomUpColumns/2
+	}
+
+}
