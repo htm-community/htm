d05df00c364e5111414b206a54262ae3289657f0
Zac Gross <zacgross@gmail.com>
2014-07-30 15:47:52 -0400

    adding temporal memory file

---
+++ temporalMemory.go
@@ -0,0 +1,12 @@
+package htm
+
+// import (
+// 	"fmt"
+// 	"github.com/cznic/mathutil"
+// 	"github.com/zacg/floats"
+// 	"github.com/zacg/go.matrix"
+// 	"github.com/zacg/htm/utils"
+// 	//"math"
+// 	"math/rand"
+// 	//"sort"
+// )
