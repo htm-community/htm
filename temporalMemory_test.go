e165b7076aeec0da73d02af8ca701e2fd0d0aee6
Zac Gross <zacgross@gmail.com>
2014-08-29 01:23:12 -0400

    add tm dupe unit test

--- temporalMemory_test.go
+++ temporalMemory_test.go
@@ -1,23 +1,23 @@
 package htm
 
-// import (
-// 	"fmt"
-// 	"github.com/zacg/testify/assert"
-// 	"testing"
-// )
+import (
+	//"fmt"
+	"github.com/zacg/testify/assert"
+	"testing"
+)
 
-// func TestPlo(t *testing.T) {
-// 	tmp := NewTemporalMemoryParams()
-// 	tm := NewTemporalMemory(tmp)
+func TestPickCellsToLearnOnAvoidDuplicates(t *testing.T) {
+	tmp := NewTemporalMemoryParams()
+	tmp.MaxNewSynapseCount = 1000
+	tm := NewTemporalMemory(tmp)
 
-// 	connections := tm.Connections
-// 	connections.CreateSegment(0)
-// 	connections.CreateSynapse(0, 23, 0.6)
+	connections := tm.Connections
+	connections.CreateSegment(0)
+	connections.CreateSynapse(0, 23, 0.6)
 
-// 	winnerCells := []int{233, 144}
+	winnerCells := []int{233, 144}
 
-// 	// Ensure that no additional (duplicate) cells were picked
-// 	assert.Equal(t, winnerCells, tm.pickCellsToLearnOn(2, 0, winnerCells, connections))
-// 	//self.assertEqual(tm.pickCellsToLearnOn(2, 0, winnerCells, connections),
-// 	//set())
-// }
+	// Ensure that no additional (duplicate) cells were picked
+	assert.Equal(t, winnerCells, tm.pickCellsToLearnOn(2, 0, winnerCells, connections))
+
+}
