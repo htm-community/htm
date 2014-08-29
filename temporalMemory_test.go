3ef39536d4d1db8541aa26569a3beb2502d689c1
Zac Gross <zacgross@gmail.com>
2014-08-29 01:37:20 -0400

    add pick cells unit test

--- temporalMemory_test.go
+++ temporalMemory_test.go
@@ -3,6 +3,7 @@ package htm
 import (
 	//"fmt"
 	"github.com/zacg/testify/assert"
+	"sort"
 	"testing"
 )
 
@@ -20,4 +21,19 @@ func TestPickCellsToLearnOnAvoidDuplicates(t *testing.T) {
 	// Ensure that no additional (duplicate) cells were picked
 	assert.Equal(t, winnerCells, tm.pickCellsToLearnOn(2, 0, winnerCells, connections))
 
+}
+
+func TestPickCellsToLearnOn(t *testing.T) {
+	tmp := NewTemporalMemoryParams()
+	tm := NewTemporalMemory(tmp)
+	connections := tm.Connections
+	connections.CreateSegment(0)
+
+	winnerCells := []int{4, 47, 58, 93}
+
+	result := tm.pickCellsToLearnOn(100, 0, winnerCells, connections)
+	sort.Ints(result)
+	assert.Equal(t, []int{4, 47, 58, 93}, result)
+	assert.Equal(t, []int{}, tm.pickCellsToLearnOn(0, 0, winnerCells, connections))
+	assert.Equal(t, []int{4, 58}, tm.pickCellsToLearnOn(2, 0, winnerCells, connections))
 }
