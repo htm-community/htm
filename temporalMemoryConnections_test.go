b84a4f6096f9152ec39385d57150a679d74826b4
Zac Gross <zacgross@gmail.com>
2014-08-28 23:49:21 -0400

    add tmc testNumCells unit test

--- temporalMemoryConnections_test.go
+++ temporalMemoryConnections_test.go
@@ -9,3 +9,8 @@ func TestNumColumns(t *testing.T) {
 	c := NewTemporalMemoryConnections(0, 32, []int{64, 64})
 	assert.Equal(t, 64*64, c.NumberOfColumns())
 }
+
+func TestNumCells(t *testing.T) {
+	c := NewTemporalMemoryConnections(0, 32, []int{64, 64})
+	assert.Equal(t, 64*64*32, c.NumberOfcells())
+}
