72c6b95214fed3c27ad5e5278d5985118e30855b
Zac Gross <zacgross@gmail.com>
2014-08-29 00:33:16 -0400

    added permenance update unit test

--- temporalMemoryConnections_test.go
+++ temporalMemoryConnections_test.go
@@ -14,3 +14,11 @@ func TestNumCells(t *testing.T) {
 	c := NewTemporalMemoryConnections(0, 32, []int{64, 64})
 	assert.Equal(t, 64*64*32, c.NumberOfcells())
 }
+
+func TestUpdateSynapsePermanence(t *testing.T) {
+	c := NewTemporalMemoryConnections(1000, 32, []int{64, 64})
+	c.CreateSegment(0)
+	c.CreateSynapse(0, 483, 0.1284)
+	c.UpdateSynapsePermanence(0, 0.2496)
+	assert.Equal(t, 0.2496, c.DataForSynapse(0).Permanence)
+}
