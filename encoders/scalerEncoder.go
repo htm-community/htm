1287707a0b469bfbc1c3d383bfe70a7134801ed5
Zac Gross <zacgross@gmail.com>
2015-02-09 21:46:32 -0500

    implementing getFirstOnBit()

--- encoders/scalerEncoder.go
+++ encoders/scalerEncoder.go
@@ -1,6 +1,9 @@
 package encoders
 
-import ()
+import (
+	"fmt"
+	//"math"
+)
 
 /*
  n -- The number of bits in the output. Must be greater than or equal to w
@@ -40,9 +43,19 @@ type ScalerEncoder struct {
 	MaxVal        float64
 	Periodic      bool
 	OutputType    ScalerOutputType
+	Range         float64
+	Name          string
+	Radius        float64
 	padding       int
 	halfWidth     int
 	rangeInternal float64
+	clipInput     bool
+	resolution    float64
+	n             int
+	//nInternal represents the output area excluding the possible padding on each
+	nInternal int
+
+	Verbosity int
 }
 
 func NewScalerEncoder(width int) *ScalerEncoder {
@@ -59,6 +72,74 @@ func NewScalerEncoder(width int) *ScalerEncoder {
 	return se
 }
 
-func (se *ScalerEncoder) Encode(input []string, output []bool) {
+/* Return the bit offset of the first bit to be set in the encoder output.
+For periodic encoders, this can be a negative number when the encoded output
+wraps around. */
+func (se *ScalerEncoder) getFirstOnBit(input float64) int {
 
+	//if input == SENTINEL_VALUE_FOR_MISSING_DATA:
+	//	return [None]
+	//else:
+
+	if input < se.MinVal {
+		//Don't clip periodic inputs. Out-of-range input is always an error
+		if se.clipInput && !se.Periodic {
+
+			if se.Verbosity > 0 {
+				fmt.Printf("Clipped input %v=%d to minval %d", se.Name, input, se.MinVal)
+			}
+			input = se.MinVal
+		} else {
+			panic(fmt.Sprintf("Input %v less than range %v - %v", input, se.MinVal, se.MaxVal))
+		}
+
+		if se.Periodic {
+
+			// Don't clip periodic inputs. Out-of-range input is always an error
+			if input >= se.MaxVal {
+				panic(fmt.Sprintf("input %v greater than periodic range %v - %v", input, se.MinVal, se.MaxVal))
+			}
+
+		} else {
+
+			if input > se.MaxVal {
+				if se.clipInput {
+					if se.Verbosity > 0 {
+						fmt.Printf("Clipped input %v=%v to maxval %v", se.Name, input, se.MaxVal)
+					}
+					input = se.MaxVal
+				} else {
+					panic(fmt.Sprintf("input %v greater than range (%v - %v)", input, se.MinVal, se.MaxVal))
+				}
+			}
+		}
+	}
+
+	centerbin := 0
+
+	if se.Periodic {
+		centerbin = int((input-se.MinVal)*float64(se.nInternal)/se.Range) + se.padding
+	} else {
+		centerbin = int(((input-se.MinVal)+se.resolution/2)/se.resolution) + se.padding
+	}
+
+	// We use the first bit to be set in the encoded output as the bucket index
+	minbin := centerbin - se.halfWidth
+	return minbin
 }
+
+// func (se *ScalerEncoder) Encode(input float64, learn bool) output []bool {
+
+// 	// if input is not None and not isinstance(input, numbers.Number):
+// 	// raise TypeError("Expected a scalar input but got input of type %s" % type(input))
+
+// 	// if type(input) is float and math.isnan(input):
+// 	// input = SENTINEL_VALUE_FOR_MISSING_DATA
+// 	// // Get the bucket index to use
+// 	// bucketIdx = self._getFirstOnBit(input)[0]
+// 	return []bool
+// }
+
+// func (se *ScalerEncoder) GetWidth() int {
+// 	//return
+// }
