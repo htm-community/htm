75c3f808e0bd5c9aca1a7a9c67787e7fc0c8c265
Zac Gross <zacgross@gmail.com>
2015-02-21 17:54:52 -0500

    decode() unit test

--- encoders/scalerEncoder_test.go
+++ encoders/scalerEncoder_test.go
@@ -36,3 +36,49 @@ func TestSimpleEncoding(t *testing.T) {
 	assert.Equal(t, expected, encoded)
 
 }
+
+func TestSimpleDecoding(t *testing.T) {
+
+	p := NewScalerEncoderParams(3, 1, 8)
+	//p.Resolution = 1
+	//p.N = 14
+	p.Radius = 1.5
+	//p.Width = 3
+	//p.MaxVal = 8
+	//p.MinVal = 1
+	p.Periodic = true
+	p.Verbosity = 5
+
+	e := NewScalerEncoder(p)
+
+	// Test with a "hole"
+	encoded := utils.Make1DBool([]int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0})
+	expected := []utils.TupleFloat{utils.TupleFloat{7.5, 7.5}}
+	actual := e.Decode(encoded)
+	assert.Equal(t, expected, actual)
+
+	// Test with something wider than w, and with a hole, and wrapped
+	encoded = utils.Make1DBool([]int{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0})
+	expected = []utils.TupleFloat{utils.TupleFloat{7.5, 8}}
+	actual = e.Decode(encoded)
+	assert.Equal(t, expected, actual)
+
+	// Test with something wider than w, no hole
+	encoded = utils.Make1DBool([]int{1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	expected = []utils.TupleFloat{utils.TupleFloat{1.5, 2.5}}
+	actual = e.Decode(encoded)
+	assert.Equal(t, expected, actual)
+
+	// 1
+	encoded = utils.Make1DBool([]int{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1})
+	expected = []utils.TupleFloat{utils.TupleFloat{1, 1}}
+	actual = e.Decode(encoded)
+	assert.Equal(t, expected, actual)
+
+	// 2
+	encoded = utils.Make1DBool([]int{0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	expected = []utils.TupleFloat{utils.TupleFloat{2, 2}}
+	actual = e.Decode(encoded)
+	assert.Equal(t, expected, actual)
+
+}
