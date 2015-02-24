45aa676643be2f5c4a03f48e8ac1c54115064528
Zac Gross <zacgross@gmail.com>
2015-02-23 22:00:06 -0500

    adding date encoder file

---
+++ encoders/dateEncoder.go
@@ -0,0 +1,11 @@
+package encoders
+
+import (
+	"fmt"
+	//"github.com/cznic/mathutil"
+	//"github.com/zacg/floats"
+	"github.com/nupic-community/htm"
+	"github.com/nupic-community/htm/utils"
+	"github.com/zacg/ints"
+	"math"
+)
