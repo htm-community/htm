80fcddd687916d94dfb8f087eace44c03144202d
Zac Gross <zacgross@gmail.com>
2015-03-01 10:56:16 -0500

    add test cases

--- encoders/scalerEncoder_test.go
+++ encoders/scalerEncoder_test.go
@@ -9,11 +9,7 @@ import (
 func TestSimpleEncoding(t *testing.T) {
 
 	p := NewScalerEncoderParams(3, 1, 8)
-	//p.Resolution = 1
 	p.N = 14
-	//p.Width = 3
-	//p.MaxVal = 8
-	//p.MinVal = 1
 	p.Periodic = true
 	//p.Verbosity = 5
 
@@ -37,17 +33,47 @@ func TestSimpleEncoding(t *testing.T) {
 
 }
 
+func TestWideEncoding(t *testing.T) {
+
+	p := NewScalerEncoderParams(5, 0, 24)
+	p.Periodic = true
+	//p.Verbosity = 5
+	p.Radius = 4
+	e := NewScalerEncoder(p)
+
+	encoded := e.Encode(14.916666666666666, false)
+	t.Log(encoded)
+	expected := utils.Make1DBool([]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+
+	assert.True(t, len(encoded) == 30)
+	assert.Equal(t, utils.Bool2Int(expected), utils.Bool2Int(encoded))
+
+}
+
+func TestNarrowEncoding(t *testing.T) {
+
+	p := NewScalerEncoderParams(3, 0, 1)
+	p.Periodic = false
+	//p.Verbosity = 5
+	p.Radius = 1
+	e := NewScalerEncoder(p)
+
+	encoded := make([]bool, 6)
+	e.EncodeToSlice(0, false, encoded)
+	t.Log(encoded)
+	expected := utils.Make1DBool([]int{1, 1, 1, 0, 0, 0})
+
+	assert.True(t, len(encoded) == 6)
+	assert.Equal(t, utils.Bool2Int(expected), utils.Bool2Int(encoded))
+
+}
+
 func TestSimpleDecoding(t *testing.T) {
 
 	p := NewScalerEncoderParams(3, 1, 8)
-	//p.Resolution = 1
-	//p.N = 14
 	p.Radius = 1.5
-	//p.Width = 3
-	//p.MaxVal = 8
-	//p.MinVal = 1
 	p.Periodic = true
-	p.Verbosity = 5
+	//p.Verbosity = 5
 
 	e := NewScalerEncoder(p)
 
