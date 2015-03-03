e5777ef13dd4fee6b59ac883b9600cb234ecae28
Zac Gross <zacgross@gmail.com>
2015-03-02 19:32:47 -0500

    adding sequence file

---
+++ sequence.go
@@ -0,0 +1,36 @@
+package encoders
+
+import (
+	"fmt"
+	//"github.com/cznic/mathutil"
+	//"github.com/zacg/floats"
+	//"github.com/nupic-community/htm"
+	"github.com/nupic-community/htm/utils"
+	//"github.com/zacg/ints"
+	//"math"
+	"time"
+)
+
+type Sequence struct {
+	data []uint64
+}
+
+func (s *Sequence) Append(other Sequence) Sequence {
+
+}
+
+func (s *Sequence) Or(other Sequence) Sequence {
+
+}
+
+func (s *Sequence) And(other Sequence) Sequence {
+
+}
+
+func (s *Sequence) At(idx int) bool {
+
+}
+
+func (s *Sequence) Set(idx int, val bool) {
+
+}
