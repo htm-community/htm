05efdcc66054c26c168f6efa8498678ac6875274
Zac Gross <zacgross@gmail.com>
2015-03-09 22:10:57 -0400

    implement initiializers

--- sequence.go
+++ sequence.go
@@ -1,4 +1,4 @@
-package encoders
+package htm
 
 import (
 	"fmt"
@@ -7,23 +7,81 @@ import (
 	//"github.com/nupic-community/htm"
 	"github.com/nupic-community/htm/utils"
 	//"github.com/zacg/ints"
-	//"math"
+	"math"
 	"time"
 )
 
 type Sequence struct {
 	data []uint64
+	binaryLength int
 }
 
-func (s *Sequence) Append(other Sequence) Sequence {
+/* Intializers */
+
+func BinarySequence(int length) *Sequence {
+	seq := new(Sequence)
+	seq.init(length)
+	return seq
+}
+
+func FromInts(ints []int) *Sequence {
+	seq := new(Sequence)
+	seq.init(len(ints))
+	for idx,val := range ints {
+		if(val){
+			seq.Set(idx, true)
+		}
+	}
+	return seq
+}
+
+func FromStr(str string) *Sequence {
+	seq := new(Sequence)
+	seq.init(len(str))
+	for idx,val := range ints {
+		if(val != "0"){
+			seq.Set(idx, true)
+		}
+	}
+	return seq
+}
+
+func Ones(int length) *Sequence {
+	seq := new(Sequence)
+	seq.init(length)
+	for idx,val := range seq.data {
+		seq.data[idx] = math.MaxUint64
+	}
+	return seq
+}
+
+/* *************************************/
+
+/* helpers */
+
+func (s *Sequence) init(size int) {
+	data = make([]uint64,s.idx(size)+1)
+}
+
+func (s *Sequence) idx(i index) int {
+	return i \ 64
+}
+
+/* exported functions */
+
+func (s *Sequence) Equals(other Sequence) bool {
 
 }
 
-func (s *Sequence) Or(other Sequence) Sequence {
+func (s *Sequence) Append(other Sequence) *Sequence {
 
 }
 
-func (s *Sequence) And(other Sequence) Sequence {
+func (s *Sequence) Or(other Sequence) *Sequence {
+
+}
+
+func (s *Sequence) And(other Sequence) *Sequence {
 
 }
 
@@ -32,5 +90,29 @@ func (s *Sequence) At(idx int) bool {
 }
 
 func (s *Sequence) Set(idx int, val bool) {
+
+}
+
+func (s *Sequence) Len() int {
+	return s.binaryLength
+}
+
+func (s *Sequence) SetIndices(idx []int, val bool) {
+
+}
+
+func (s *Sequence) OnIndices() []int {
+
+}
+
+func (s *Sequence) Contains(other *Sequence) bool {
+
+}
+
+func (s *Sequence) String() string {
+
+}
+
+func (s *Sequence) Slice() []bool {
 
 }
